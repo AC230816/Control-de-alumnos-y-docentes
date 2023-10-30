ba32bcf4e4a059bcb382909b8d8a9f03b15df1f6
DiegoFG10 <113257329+DiegoFG10@users.noreply.github.com>
2023-10-29 19:27:26 -0600

    registroServlets

--- src/main/java/servlets/registroServlets.java
+++ src/main/java/servlets/registroServlets.java
@@ -1,4 +1,27 @@
 package servlets;
 
-public class registroServlets {
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(name = "RegistrosuarioServlet", value = "/RegistrosuarioServlet")
+public class registroServlets extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String ocupacion = request.getParameter("ocupacion");
+        String materia = request.getParameter("materia");
+        int edad = Integer.parseInt(request.getParameter("edad"));
+        String sexo = request.getParameter("sexo");
+        String password = request.getParameter("password");
+        String confirm_password = request.getParameter("confirm_password");
+
+        System.out.println(nombre);
+
+        response.sendRedirect("login.jsp");
+    }
 }
