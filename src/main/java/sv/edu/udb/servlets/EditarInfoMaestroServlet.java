3f723242737795fd7440452dd8da2ccc0cbc7513
AC230816 <AC230816@alumno.udb.edu.sv>
2023-11-01 19:41:57 -0600

    Se agrega el CRUD de maestro

---
+++ src/main/java/sv/edu/udb/servlets/EditarInfoMaestroServlet.java
@@ -0,0 +1,16 @@
+package sv.edu.udb.servlets;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class EditarInfoMaestroServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
